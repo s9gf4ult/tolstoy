6110914e97807ca9a4ec789f2a4de9e777982339
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-10 11:46:25 +0500

    wip

---
+++ migrations/init.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE documents (
+  id                 uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  created_at         timestamp with time zone DEFAULT NOW() NOT NULL,
+);
+
+CREATE TABLE history (
+  id                 uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  created_at         timestamp with time zone DEFAULT NOW() NOT NULL,
+  seqnum             bigserial NOT NULL,
+  -- Seqnum is a seqence number to select the last version of each
+  -- document very fast.
+  parrent_id         uuid NOT NULL REFERENCES history(id) UNIQUE,
+  -- The link to the parrent history record. Must be uniq, so all the
+  -- history records form a linked list with strict order. The order
+  -- in linked list is more important than order formed by seqnum. The
+  -- seqnum is oly for listing documents.
+  document           jsonb NOT NULL,
+  action             jsonb NOT NULL,
+);
--- src/Tolstoy/DB.hs
+++ src/Tolstoy/DB.hs
@@ -1,19 +1,65 @@
 module Tolstoy.DB where
 
-import           Data.Aeson
-import           Data.Text                 as T
-import           Database.PostgreSQL.Query
+import Data.Aeson
+import Data.Pool
+import Data.Text as T
+import Data.Time
+import Database.PostgreSQL.Query as PG
+import GHC.Generics (Generic)
 
-type Action obj act = obj -> act -> Either Text obj
+type Error = Text
 
-data Tolstoy obj objId act = Tolstoy
-  { insertObject :: obj -> IO objId
-  , getObject    :: objId -> IO obj
-  , changeObject :: objId -> act -> IO obj
-  }
+type Action doc act = doc -> act -> Either Error doc
+
+newtype Seqnum = Seqnum
+  { unSeqnum :: Integer
+  } deriving (Eq, Ord, Show, FromField, ToField, Generic)
+
+data DocDesc doc docId actId = DocDesc
+  { doc      :: doc
+  , docId    :: docId
+  , actionId :: actId
+  , seqnum   :: Seqnum
+  , created  :: UTCTime
+  , modified :: UTCTime
+  } deriving (Eq, Ord, Show, Generic)
+
+data Tolstoy doc docId act actId = Tolstoy
+  { newDoc
+    :: doc
+    -> IO (DocDesc doc docId actId)
+  -- ^ Inserts a new document in DB
+  , getDoc
+    :: docId
+    -> IO (Maybe (DocDesc doc docId actId))
+  -- ^ Get last version of some object
+  , changeDoc
+    :: DocDesc doc docId actId
+    -> act
+    -> IO (Either Error (DocDesc doc docId actId))
+  -- ^ Saves changed doc to the DB. Note that it does not check the
+  -- document history consistency right now.
+  } deriving (Generic)
+
+data TolstoyInit doc docId act actId = TolstoyInit
+  { exec           :: Action doc docId
+  , dbPool         :: Pool PG.Connection
+  , documentsTable :: FN
+  , actionsTable   :: FN
+  } deriving (Generic)
+
+runPG
+  :: TolstoyInit doc docId act actId
+  -> (HasCallStack => PgMonadT IO a)
+  -> IO a
+runPG = error "FIXME: runPG not implemented"
 
 tolstoy
-  :: forall act obj objId
-  .  (Action obj act)
-  -> Tolstoy act obj objId
-tolstoy = error "FIXME: tolstoy not implemented"
+  :: forall doc docId act actId
+  .  (TolstoyInit doc docId act actId)
+  -> Tolstoy act doc docId actId
+tolstoy init = Tolstoy { newDoc, getDoc, changeDoc }
+  where
+    docs = documentsTable
+    newDoc = runPG init $ do
+      pgQuery [sqlExp|insert into ^{docs} returning id, created_at|]
