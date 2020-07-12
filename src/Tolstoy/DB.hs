9d3e52742d8992c5b68b825c4be4d4703a20c95f
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-12 14:53:38 +0500

    wip

--- src/Tolstoy/DB.hs
+++ src/Tolstoy/DB.hs
@@ -1,5 +1,6 @@
 module Tolstoy.DB where
 
+import Control.Monad.Fail
 import Data.Aeson
 import Data.Pool
 import Data.Text as T
@@ -38,6 +39,8 @@ instance (FromJSON a, Typeable a) => FromField (JsonField a) where
 instance (ToJSON a) => ToField (JsonField a) where
   toField = toJSONField . unJsonField
 
+type JSON a = (FromJSON a, ToJSON a, Typeable a)
+
 data DocDesc doc act = DocDesc
   { doc      :: doc
   , docId    :: DocId
@@ -48,49 +51,45 @@ data DocDesc doc act = DocDesc
   , modified :: UTCTime
   } deriving (Eq, Ord, Show, Generic)
 
-data Tolstoy doc act = Tolstoy
+data Tolstoy m doc act = Tolstoy
   { newDoc
     :: doc
     -- ^ Initial state of the doc.
     -> act
     -- ^ Initial action. It will not be performed on given doc, only
     -- written to DB
-    -> IO (DocDesc doc act)
+    -> m (DocDesc doc act)
   -- ^ Inserts a new document in DB
   , getDoc
     :: DocId
-    -> IO (Maybe (DocDesc doc act))
+    -> m (Maybe (DocDesc doc act))
   -- ^ Get last version of some object
   , changeDoc
     :: DocDesc doc act
     -> act
-    -> IO (Either Error (DocDesc doc act))
+    -> m (Either Error (DocDesc doc act))
   -- ^ Saves changed doc to the DB. Note that it does not check the
   -- document history consistency right now.
   } deriving (Generic)
 
 data TolstoyInit doc act = TolstoyInit
   { docAction      :: DocAction doc act
-  , dbPool         :: Pool PG.Connection
   , documentsTable :: FN
   , actionsTable   :: FN
   } deriving (Generic)
 
-runPG
-  :: TolstoyInit doc act
-  -> (HasCallStack => PgMonadT IO a)
-  -> IO a
-runPG = error "FIXME: runPG not implemented"
-
 tolstoy
-  :: forall doc act
-  .  (TolstoyInit doc act)
-  -> Tolstoy doc act
+  :: forall m doc act
+  .  ( MonadPostgres m
+     , MonadFail m              -- FIXME: KILL it
+     , JSON doc, JSON act)
+  => (TolstoyInit doc act)
+  -> Tolstoy m doc act
 tolstoy init =
   Tolstoy { newDoc, getDoc, changeDoc }
   where
     docs = documentsTable
-    newDoc doc act = runPG init $ do
+    newDoc doc act = do
       [(docId, created)] <- pgQuery
         [sqlExp|INSERT INTO ^{documentsTable init} RETURNING id, created_at|]
       [(actId, modified, seqnum)] <- pgQuery
@@ -98,8 +97,45 @@ tolstoy init =
                 VALUES ( #{docId}, #{JsonField doc}, #{JsonField act} )
                 RETURNING id, created_at, seqnum|]
       let
-        res = DocDesc {doc, docId, act, actId, seqnum, created, modified }
+        res = DocDesc { doc, docId, act, actId, seqnum, created, modified }
       return res
-
-    getDoc = (error "FIXME: not implemented")
-    changeDoc = (error "FIXME: not implemented")
+    getDoc docId = do
+      res <- pgQuery
+        [sqlExp|SELECT
+        doc.id as doc_id,
+        doc.created_at as created,
+        act.id as act_id,
+        act.created_at as modified,
+        act.seqnum,
+        act.document,
+        act.action
+        FROM ^{documentsTable init} as doc INNER JOIN ^{actionsTable init} as act
+          ON doc.id = act.document_id
+        WHERE doc.id = #{docId}
+        |]
+      case res of
+        [] -> return Nothing
+        [( docId
+         , created
+         , actId
+         , modified
+         , seqnum
+         , JsonField doc
+         , JsonField act)] -> return $ Just $ DocDesc {..}
+    changeDoc docDesc act = case docAction init (doc docDesc) act of
+      Left e -> return $ Left e
+      Right newDoc -> do
+        res <- pgQuery [sqlExp|
+          INSERT INTO ^{actionsTable init} (document_id, parent_id, document, action)
+          VALUES (#{docId docDesc}, #{actId docDesc}, #{JsonField newDoc}, #{JsonField act})
+          RETURNING id, created_at, seqnum|]
+        case res of
+          [(newActId, newMod, newSeq)] -> return $ Right $ DocDesc
+            { doc      = newDoc
+            , docId    = docId docDesc
+            , act      = act
+            , actId    = newActId
+            , seqnum   = newSeq
+            , created  = created docDesc
+            , modified = newMod
+            }
