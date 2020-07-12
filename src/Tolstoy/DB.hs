b8758d6c5a8aec7200ee542009cbc1528f3314f7
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-12 21:33:04 +0500

    mkay

--- src/Tolstoy/DB.hs
+++ src/Tolstoy/DB.hs
@@ -1,17 +1,23 @@
 module Tolstoy.DB where
 
-import Control.Monad.Fail
-import Data.Aeson
-import Data.Pool
-import Data.Text as T
-import Data.Time
-import Data.Typeable
-import Data.UUID.Types
-import Database.PostgreSQL.Query as PG
-import Database.PostgreSQL.Simple.FromField
-import Database.PostgreSQL.Simple.ToField
-import GHC.Generics (Generic)
-import GHC.Stack
+import           Control.Arrow
+import           Control.Lens
+import           Control.Monad.Fail
+import           Data.Aeson
+import           Data.Generics.Product
+import           Data.List.NonEmpty as NE
+import qualified Data.Map as M
+import           Data.Pool
+import           Data.Text as T
+import           Data.Time
+import           Data.Typeable
+import           Data.UUID.Types
+import           Database.PostgreSQL.Query as PG
+import           Database.PostgreSQL.Simple.FromField
+import qualified Database.PostgreSQL.Simple.FromRow as PG
+import           Database.PostgreSQL.Simple.ToField
+import           GHC.Generics (Generic)
+import           GHC.Stack
 
 type Error = Text
 
@@ -47,15 +53,73 @@ instance (ToJSON a) => ToField (JsonField a) where
 type JSON a = (FromJSON a, ToJSON a, Typeable a)
 
 data DocDesc doc act = DocDesc
-  { doc      :: JsonField doc
+  { doc      :: doc
   , docId    :: DocId doc
-  , act      :: JsonField act
+  , act      :: act
   , actId    :: ActId act
   , created  :: UTCTime
   , modified :: UTCTime
   } deriving (Eq, Ord, Show, Generic)
 
-instance (JSON doc, JSON act) => FromRow (DocDesc doc act)
+instance (JSON doc, JSON act) => FromRow (DocDesc doc act) where
+  fromRow = do
+    JsonField doc <- PG.field
+    docId <- PG.field
+    JsonField act <- PG.field
+    actId <- PG.field
+    created <- PG.field
+    modified <- PG.field
+    return $ DocDesc {..}
+
+data DocHistory doc act = DocHistory
+  { docId   :: DocId doc
+  , created :: UTCTime
+  , history :: NonEmpty (Story doc act)
+  -- ^ Story points in reverse order. Head is the last actual version
+  -- and tail is the initial
+  } deriving (Eq, Ord, Show, Generic)
+
+data Story doc act = Story
+  { doc      :: doc
+  , act      :: act
+  , actId    :: ActId act
+  , modified :: UTCTime
+  } deriving (Eq, Ord, Show, Generic)
+
+data ActionRow doc act = ActionRow
+  { actionId :: ActId act
+  , created  :: UTCTime
+  , parentId :: ActId act
+  , document :: doc
+  , action   :: act
+  } deriving (Eq, Ord, Show, Generic)
+
+instance (JSON doc, JSON act) => FromRow (ActionRow doc act) where
+  fromRow = do
+    actionId <- PG.field
+    created <- PG.field
+    parentId <- PG.field
+    JsonField document <- PG.field
+    JsonField action <- PG.field
+    return $ ActionRow {..}
+
+actionsHistory
+  :: forall doc act
+  .  ActId act
+  -> [ActionRow doc act]
+  -> Maybe (NonEmpty (Story doc act))
+actionsHistory a actions = NE.nonEmpty $ go a
+  where
+    go actId = case M.lookup actId actMap of
+      Nothing -> []
+      Just h  -> toStory h : (go $ h ^. field @"parentId")
+    toStory h = Story
+      { doc = h ^. field @"document"
+      , act = h ^. field @"action"
+      , actId = h ^. field @"actionId"
+      , modified = h ^. field @"created" }
+    actMap :: M.Map (ActId act) (ActionRow doc act)
+    actMap = M.fromList $ (view (field @"actionId") &&& id) <$> actions
 
 data Tolstoy m doc act a = Tolstoy
   { newDoc
@@ -70,6 +134,10 @@ data Tolstoy m doc act a = Tolstoy
     :: DocId doc
     -> m (Maybe (DocDesc doc act))
   -- ^ Get last version of some object
+  , getDocHistory
+    :: DocId doc
+    -> m (Maybe (DocHistory doc act))
+  -- ^ Get full history of the document
   , changeDoc
     :: DocDesc doc act
     -> act
@@ -93,7 +161,7 @@ tolstoy
   => (TolstoyInit doc act a)
   -> Tolstoy m doc act a
 tolstoy init =
-  Tolstoy { newDoc, getDoc, changeDoc, documentsList }
+  Tolstoy { newDoc, getDoc, getDocHistory, changeDoc, documentsList }
   where
     docs = documentsTable
     documentsList = [sqlExp|SELECT
@@ -117,11 +185,7 @@ tolstoy init =
         RETURNING id, created_at|]
       let
         res = DocDesc
-          { doc = JsonField doc
-          , docId
-          , act = JsonField act
-          , actId
-          , created
+          { doc , docId , act , actId , created
           , modified = created }
       return res
     getDoc docId = do
@@ -131,22 +195,47 @@ tolstoy init =
         []        -> return Nothing
         [docDesc] -> return $ Just docDesc
         _         -> error "Unexpected count of results"
-    changeDoc docDesc act = case docAction init (unJsonField $ doc docDesc) act of
+    getDocHistory docId = do
+      res <- pgQuery [sqlExp|SELECT created_at, action_id
+        FROM ^{documentsTable init}
+        WHERE id = #{docId}|]
+      case res of
+        [] -> return Nothing
+        [(created, actId)] -> do
+          actions <- pgQuery [sqlExp|
+            WITH RECURSIVE result(id, created_at, parrent_id, document, action) AS (
+              SELECT id, created_at, parrent_id, document, action
+              FROM ^{actionsTable init}
+              WHERE id = #{actId}
+            UNION ALL
+              SELECT act.id, act.created_at, act.parrent_id, act.document, act.action
+              FROM ^{actionsTable init} AS act INNER JOIN result AS res
+                ON res.parrent_id = act.id
+            ) SELECT * FROM result |]
+          case actionsHistory actId actions of
+            Nothing      -> error "No actions. Unexpected result"
+            Just history -> return $ Just
+              $ DocHistory {docId, created, history}
+
+    changeDoc docDesc act = case docAction init (docDesc ^. field @"doc") act of
       Left e       -> return $ Left e
       Right (newDoc, a) -> do
+        let
+          parent = docDesc ^. field @"actId"
+          docId = docDesc ^. field @"docId"
         [(newActId, newMod)] <- pgQuery [sqlExp|
           INSERT INTO ^{actionsTable init} (parent_id, document, action)
-          VALUES ( #{actId docDesc}, #{JsonField newDoc}, #{JsonField act})
+          VALUES ( #{parent}, #{JsonField newDoc}, #{JsonField act})
           RETURNING id, created_at|]
         1 <- pgExecute [sqlExp|UPDATE ^{documentsTable init}
           SET action_id = #{newActId}
-          WHERE id = #{docId docDesc}|]
+          WHERE id = #{docId}|]
         let
           res =  DocDesc
-            { doc      = JsonField newDoc
-            , docId    = docId docDesc
-            , act      = JsonField act
+            { doc      = newDoc
+            , docId    = docId
+            , act      = act
             , actId    = newActId
-            , created  = created docDesc
+            , created  = docDesc ^. field @"created"
             , modified = newMod }
         return $ Right (res, a)
