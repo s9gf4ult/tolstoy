f79194ce6b600711347eb6cd805bcc231627bfcf
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-22 17:22:36 +0500

    fix

--- src/Tolstoy/Structure/Class.hs
+++ src/Tolstoy/Structure/Class.hs
@@ -1,1 +1,9 @@
 module Tolstoy.Structure.Class where
+
+import Tolstoy.Structure.Kind
+import Tolstoy.Structure.Value
+
+class Structural s where
+  type StructKind s :: Structure
+  toStructValue :: s -> StructureValue (StructKind s)
+  fromStructValue :: StructureValue (StructKind s) -> s
