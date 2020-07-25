2c988f9bc6c4daabffab80fe9ce73243224ee7a9
Aleksey Uymanov <s9gf4ult@gmail.com>
2020-07-26 00:26:27 +0500

    tests done

--- test/StructureValue.hs
+++ test/StructureValue.hs
@@ -19,9 +19,11 @@ import Data.Vector as V
 import Database.PostgreSQL.Query as PG
 import Database.PostgreSQL.Simple as PG
 import GHC.Generics (Generic)
+import Prelude as P
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Arbitrary.Generic
 import Test.QuickCheck.Gen as Gen
+import Test.QuickCheck.Instances ()
 import Test.Tasty as Test
 import Test.Tasty.HUnit as Test
 import Test.Tasty.QuickCheck
@@ -64,8 +66,8 @@ instance Arbitrary SomeStructureValue where
   arbitrary = oneof allCases
     where
       allCases =
-        [ SomeStructureValue . StringValue <$> arbText
-        , SomeStructureValue . NumberValue <$> arbScientific
+        [ SomeStructureValue . StringValue <$> arbitrary
+        , SomeStructureValue . NumberValue <$> arbitrary
         , SomeStructureValue . BoolValue <$> arbitrary
         , opt
         , vec
@@ -92,9 +94,37 @@ instance Arbitrary SomeStructureValue where
           Gen (ProductValueL '[ '("some", s1), '("other", s2) ])
         return $ SomeStructureValue $ ProductValue p
 
+someStructure :: Gen SomeStructureValue
+someStructure = suchThat arbitrary noJustNothing
+
+noJustNothing :: SomeStructureValue -> Bool
+noJustNothing (SomeStructureValue a) = go a
+  where
+    go :: StructureValue s -> Bool
+    go = \case
+      StringValue {} -> True
+      NumberValue {} -> True
+      BoolValue {}   -> True
+      OptionalValue (Just sub) -> case sub of
+        OptionalValue Nothing -> False
+        _                     -> go sub
+      OptionalValue Nothing -> True
+      VectorValue v -> P.all go v
+      SumValue s -> goSum s
+      ProductValue p -> goProd p
+    goSum :: SumValueL l -> Bool
+    goSum = \case
+      ThisValue _ s -> go s
+      ThatValue rest -> goSum rest
+    goProd :: ProductValueL l -> Bool
+    goProd = \case
+      ProductNil -> True
+      ProductCons _ s rest -> go s && goProd rest
+
 test_StructureValue :: TestTree
 test_StructureValue = testGroup "Pure tests"
   [ testProperty "ToJSON/FromJSON (StructureValue s)"
+    $ forAll someStructure
     $ \(s :: SomeStructureValue) -> case s of
       SomeStructureValue (v :: StructureValue s) ->
         Just v === J.decode (J.encode v)
